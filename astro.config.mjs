97d622f39971c23315a92f524d3cefdb1963e4c3
代骏泽 <daijz5@mail2.sysu.edu.cn>
2026-03-12 20:09:21 +0800

    implement homepage basics.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      allowedHosts: [
+        "dvdbr3o-laptop", // 允许你平板上输入的那个名称
+        "laptop.local", // 某些系统下带后缀的名称
+        ".local", // 或者允许所有以 .local 结尾的主机名
+      ],
+    },
+  },
+});
